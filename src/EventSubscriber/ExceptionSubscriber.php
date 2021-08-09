431473b3981d299ec7743ee0631c42dd883fa444
Jorge Oscar Gianotti <jorge.oscar.gianotti@gmail.com>
2021-08-09 00:40:33 -0300

    Se previenen excepciones mostrando un mensaje de error

---
+++ src/EventSubscriber/ExceptionSubscriber.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+
+class ExceptionSubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents()
+    {
+        return [
+            'kernel.exception' => 'onKernelException',
+        ];
+    }
+
+    public function onKernelException(ExceptionEvent $event)
+    {
+        $event->setResponse(new Response('Se ha producido un error'));
+    }
+}
